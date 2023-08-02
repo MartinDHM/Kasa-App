6775d49e2fe950d40a4ba02c51267a1e9dacd481
MartinDHM <martin.duhem1@gmail.com>
2023-08-02 16:18:10 +0200

    Creation de l'app React pour Kasa

---
+++ kasa_app/src/components/Headers.jsx
@@ -0,0 +1,13 @@
+import logo from '../assets/logo.jpg'
+
+function headers() {
+
+return (
+
+    <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />         
+  </header>
+ 
+)
+}
+export default headers;
